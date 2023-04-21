3537a80f356211540f789b159ea0e76eea6039d2
mikaeltreppo <mikaeltreppo@outlook.com>
2023-04-21 16:00:33 +0200

    inizio implementazione

--- server.php
+++ server.php
@@ -1,4 +1,19 @@
 <?php
-$todo =['alzarsi','mangiare','uscire','divertirsi','lavarsi','dormire'];
+if (file_exists('database.json')) {
+    $string = file_get_contents('database.json');
+    $todoList = json_decode($string, true);
+} else {
+    $todoList = [];
+}
+if (isset($_POST['todoItem'])) {
+
+    $todoList[] = $_POST['todoItem'];
+
+    $todoList = json_encode($todoList);
+    file_put_contents('database.json', $todoList);
+
+}
+
+
 header('Content-Type: application/json');
-echo json_encode($todo);
+echo json_encode($todoList);
